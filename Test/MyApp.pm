1b53aee44ece5ac0e8017d91e0b563d56cbd1251
Jan C Kaldestad <janck76@gmail.com>
2015-08-23 17:16:41 +0200

    Bruk miljøvariabel MYAPP_ENV og yaml konfig
    * Miljøvariabel MYAPP_ENV styrer om modulen kjører i test/prod
      Gyldige verdier:
      - test
      - prod
    * Yaml konfig: /var/opt/myapp.yaml
      Her konfigureres variabler for test og prod, og felles
      variabler
    * funksjon print_config for å skrive ut yaml konfig

--- Test/MyApp.pm
+++ Test/MyApp.pm
@@ -2,17 +2,34 @@ package Test::MyApp;
 
 use strict;
 use warnings;
+
+use Readonly;
 use Carp;
 use Module::Metadata;
 use Exporter;
+use Smart::Comments;
+use Data::Dumper;
 use version; our $VERSION = qv('0.0.1');
 
-
-# Module implementation here	
 our @ISA        = qw(Exporter);
-our @EXPORT_OK  = qw( check_module );
-our @EXPORT     = qw( check_module );
+our @EXPORT_OK  = qw( check_module print_config );
+our @EXPORT     = qw( check_module print_config );
 
+my $CONFIG;
+my $MYAPP_ENV;
+
+BEGIN {
+    my $config = '/var/opt/myapp.yaml';
+
+    Readonly $MYAPP_ENV => lc $ENV{MYAPP_ENV};
+    die 'MYAPP_ENV undefiend' unless $MYAPP_ENV;
+    die 'MYAPP_ENV invalid value' unless $MYAPP_ENV =~ /^prod|test$/;
+
+    use YAML::Tiny;
+    my $yaml = YAML::Tiny->read($config) or die "Can't read $config: $!";
+    Readonly $CONFIG => $yaml->[0];
+    die "Config undefined" unless $CONFIG;
+}
 
 sub check_module {
     my $result = shift;
@@ -28,5 +45,11 @@ sub check_module {
     }
     return;
 }
+
+sub print_config {
+    my $common_config = $CONFIG->{common};
+    my $myapp_config = $CONFIG->{$MYAPP_ENV};
+    print Dumper $common_config;
+    print Dumper $myapp_config;
+}
  
-1;
